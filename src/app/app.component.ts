ac4767345d4d032b72654349179cb31ade0d8349
Damanjeet Singh <49544630+DamanjeetSingh1@users.noreply.github.com>
2024-03-02 17:39:08 +0530

    added Angular Material experimental package and implemented functionality to switch between Material 2 and Material 3 theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,19 +2,37 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatCardModule, MatButtonModule],
+  imports: [
+    RouterOutlet,
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatDatepickerModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   isLightMode = true;
+  isM3Theme = true;
 
-  onToggleThemeColors() {
+  onThemeColorsToggle() {
     this.isLightMode = !this.isLightMode;
     document.querySelector('body')?.classList.toggle('dark');
+  }
+
+  onThemeToggle() {
+    this.isM3Theme = !this.isM3Theme;
+    document.querySelector('body')?.classList.toggle('m3');
   }
 }
