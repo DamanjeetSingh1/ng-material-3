ede00d8d588a9f1c99460b51795cf6f794597e5a
Damanjeet Singh <49544630+DamanjeetSingh1@users.noreply.github.com>
2024-03-02 16:39:36 +0530

    added Angular Material and setup light and dark theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatCardModule, MatButtonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'ng-material-3';
+  isLightMode = true;
+
+  onToggleThemeColors() {
+    this.isLightMode = !this.isLightMode;
+    document.querySelector('body')?.classList.toggle('dark');
+  }
 }
